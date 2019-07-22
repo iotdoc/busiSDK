ed62be56095e4a68319c30392e80a3557505b011
chao01.han@horizon.ai <chao01.han@horizon.ai>
2019-07-22 15:25:37 +0800

    add comment c++ sample

--- auth/c++/sample.cpp
+++ auth/c++/sample.cpp
@@ -29,7 +29,7 @@ void addquery(string &url, Params params);
 std::string ak = "FCM5ZwyF2UFYmYTnr611SdDK";
 std::string sk = "tXIttrZeOBudgBjps5fUtSgyvyCBP3Gj";
 
-// http request sample
+// request sample function
 void PutSample();
 void GetSample();
 void PostSample();
@@ -38,6 +38,7 @@ void WebSocketSample();
 
 //main work function
 int main(int argc, char *argv[]) {
+    //提供 GET,POST,PUT,DELETE,WebSocket 多个接口的样例 
     PutSample();
     GetSample();
     PostSample();
@@ -47,31 +48,40 @@ int main(int argc, char *argv[]) {
 }
 
 void PutSample(){
-    //到店记录订阅
-    //https://iotdoc.horizon.ai/busiopenapi/part5_api_analysis_tools/statistics.html#part5_4
-    std::string server = "api-aiot.horizon.ai";
-    std::string method = "PUT";
-    std::string api = "/openapi/v1/analysis_tools/visitors/sub";
-    Headers headers;
+    // 到店记录订阅
+    // https://iotdoc.horizon.ai/busiopenapi/part5_api_analysis_tools/statistics.html#part5_4
+    // 服务器host
+    std::string server = "api-aiot.horizon.ai";  
+    // http请求方法，例如 GET,POST,PUT,DELETE
+    std::string method = "PUT";  
+    // 请求的path
+    std::string path = "/openapi/v1/analysis_tools/visitors/sub";  
+    // http请求的header，host字段必须传
+    Headers headers;  
     headers["host"] = server;
     headers["content-type"]="application/json";
-    Params params;
+    // http请求可能需要添加的params，格式如下
+    Params params;  
     //params["a"]="1";
     int ret=0;
 
+    // 生成签名字符串
     hobotpaas::HTTPProxyPaas Paas(ak,sk);
-    std::string Authorization = Paas.Sign(method, api, params, headers, ret); //签名认证生产
+    std::string Authorization = Paas.Sign(method, path, params, headers, ret); 
     if(ret != 0){
         printf("something wrong happened\n");
         return;
     }
     printf("Authorization  %s \n", Authorization.c_str());
 
-    std::string req_body = "{\"topic_name\":\"device\",\"topic_id\":\"068B3001100110057L\",\"client_id\":\"cid\"}";
+    // 请求接口需要提交的body数据
+    std::string req_body = "{\"topic_name\":\"device\",\"topic_id\":\"068B3001100110057L\",\"client_id\":\"cid\"}";  
     std::string resp;
-    std::string url = "http://" + headers["host"] + api + "?authorization=" + Authorization;
-    addquery(url, params);
-    int status_code = put(url, &resp, headers, req_body);
+    std::string url = "http://" + headers["host"] + path + "?authorization=" + Authorization;
+    // http请求需要添加之前的params
+    addquery(url, params);  
+    // 发起http请求
+    int status_code = put(url, &resp, headers, req_body);  
     if (status_code != CURLE_OK) {
 	    printf("error status code:  %d\n", status_code);
     }
@@ -82,17 +92,23 @@ void PutSample(){
 void GetSample(){
     //获取设备空间列表
     //https://iotdoc.horizon.ai/busiopenapi/part1_device_space/device_space.html#part1_0
-    std::string server = "api-aiot.horizon.ai";
-    std::string method = "GET";
-    std::string api = "/openapi/v1/device_spaces";
-    Headers headers;
+    // 服务器host
+    std::string server = "api-aiot.horizon.ai";  
+    // http请求方法，例如 GET,POST,PUT,DELETE
+    std::string method = "GET";  
+    // 请求的path
+    std::string path = "/openapi/v1/device_spaces";  
+    // http请求的header，host字段必须传
+    Headers headers;  
     headers["host"] = server;
-    Params params;
-    //params["a"]="1";
+    // http请求可能需要添加的params，格式如下
+    Params params;  
+    // params["a"]="1";
     int ret=0;
 
+    // 生成签名字符串
     hobotpaas::HTTPProxyPaas Paas(ak,sk);
-    std::string Authorization = Paas.Sign(method, api, params, headers, ret); //签名认证生产
+    std::string Authorization = Paas.Sign(method, path, params, headers, ret);
     if(ret != 0){
         printf("something wrong happened\n");
         return;
@@ -100,41 +116,52 @@ void GetSample(){
     printf("Authorization  %s \n", Authorization.c_str());
 
     std::string resp;
-    std::string url = "http://" + headers["host"] + api + "?authorization=" + Authorization;
+    std::string url = "http://" + headers["host"] + path + "?authorization=" + Authorization;
+    // http请求需要添加之前的params
     addquery(url, params);
+    // 发起http请求
     int status_code = get(url, &resp, headers);
     if (status_code != CURLE_OK) {
 	    printf("error status code:  %d\n", status_code);
     }
-    printf("result : %s", resp.c_str());
+    printf("result : %s\n", resp.c_str());
     return;
 }
 
 void PostSample(){
     //创建设备空间
     //https://iotdoc.horizon.ai/busiopenapi/part1_device_space/device_space.html#part1_1
+    // 服务器host
     std::string server = "api-aiot.horizon.ai";
-    std::string method = "POST";
-    std::string api = "/openapi/v1/device_spaces";
-    Headers headers;
+    // http请求方法，例如 GET,POST,PUT,DELETE
+    std::string method = "POST";  
+    // 请求的path
+    std::string path = "/openapi/v1/device_spaces";  
+    // http请求的header，host字段必须传
+    Headers headers;  
     headers["host"] = server;
     headers["content-type"]="application/json";
-    Params params;
+    // http请求可能需要添加的params，格式如下
+    Params params;  
     //params["a"]="1";
     int ret=0;
 
+    // 生成签名字符串
     hobotpaas::HTTPProxyPaas Paas(ak,sk);
-    std::string Authorization = Paas.Sign(method, api, params, headers, ret); //签名认证生产
+    std::string Authorization = Paas.Sign(method, path, params, headers, ret);
     if(ret != 0){
         printf("something wrong happened\n");
         return;
     }
     printf("Authorization  %s \n", Authorization.c_str());
 
+    // 请求接口需要提交的body数据
     std::string req_body = "{\"name\":\"sdktestdevice\",\"description\":\"sdk sample\",\"extra\":\"nothing\"}";
     std::string resp;
-    std::string url = "http://" + headers["host"] + api + "?authorization=" + Authorization;
+    std::string url = "http://" + headers["host"] + path + "?authorization=" + Authorization;
+    // http请求需要添加之前的params
     addquery(url, params);
+    // 发起http请求
     int status_code = post(url, &resp, headers, req_body);
     if (status_code != CURLE_OK) {
 	    printf("error status code:  %d\n", status_code);
@@ -146,17 +173,23 @@ void PostSample(){
 void DeleteSample(){
     //删除设备空间
     //https://iotdoc.horizon.ai/busiopenapi/part1_device_space/device_space.html#part1_4
-    std::string server = "api-aiot.horizon.ai";
-    std::string method = "DELETE";
-    std::string api = "/openapi/v1/device_spaces/xxxxxxxxxxx";//xxxxxxxxxxx 为之前创建设备空间返回的space_id
-    Headers headers;
+    // 服务器host
+    std::string server = "api-aiot.horizon.ai"; 
+    // http请求方法，例如 GET,POST,PUT,DELETE
+    std::string method = "DELETE";  
+    // 请求的path
+    std::string path = "/openapi/v1/device_spaces/xxxxxxxxxxx";//xxxxxxxxxxx 为之前创建设备空间返回的space_id 
+    // http请求的header，host字段必须传
+    Headers headers;  
     headers["host"] = server;
-    Params params;
+     // http请求可能需要添加的params，格式如下
+    Params params; 
     //params["a"]="1";
     int ret=0;
 
+    // 生成签名字符串
     hobotpaas::HTTPProxyPaas Paas(ak,sk);
-    std::string Authorization = Paas.Sign(method, api, params, headers, ret); //签名认证生产
+    std::string Authorization = Paas.Sign(method, path, params, headers, ret); 
     if(ret != 0){
         printf("something wrong happened\n");
         return;
@@ -164,36 +197,45 @@ void DeleteSample(){
     printf("Authorization  %s \n", Authorization.c_str());
 
     std::string resp;
-    std::string url = "http://" + headers["host"] + api + "?authorization=" + Authorization;
+    std::string url = "http://" + headers["host"] + path + "?authorization=" + Authorization;
+    // http请求需要添加之前的params
     addquery(url, params);
+    // 发起http请求
     int status_code = del(url, &resp, headers);
     if (status_code != CURLE_OK) {
 	    printf("error status code:  %d\n", status_code);
     }
-    printf("result : %s", resp.c_str());
+    printf("result : %s\n", resp.c_str());
     return;
 }
 
 void WebSocketSample(){
     //到点记录实时推送
     //https://iotdoc.horizon.ai/busiopenapi/part5_api_analysis_tools/statistics.html#part5_6
+    // 服务器host
     std::string server = "xpushservice-aiot.horizon.ai:80";//uWS源码会在header中自动生成Host，eq: "Host: " + hostname + ":" + std::to_string(port) + "\r\n"。故生成鉴权串也加port，校验通过。
-    std::string method = "GET";
-    std::string api = "/ws";
+    // http请求方法，例如 GET,POST,PUT,DELETE
+    std::string method = "GET"; 
+    // 请求的path
+    std::string path = "/ws"; 
+    // http请求的header，host字段必须传
     std::map<std::string, std::string> extraHeaders;
-    extraHeaders["host"] = server;
-    Params params;
+    extraHeaders["host"] = server;  
+    // http请求可能需要添加的params，格式如下
+    Params params;  
     //params["a"]="1";
     int ret=0;
 
+    // 生成签名字符串
     hobotpaas::HTTPProxyPaas Paas(ak,sk);
-    std::string Authorization = Paas.Sign(method, api, params, extraHeaders, ret);
+    std::string Authorization = Paas.Sign(method, path, params, extraHeaders, ret);
     if(ret != 0){
         printf("something wrong happened\n");
         return;
     }
     printf("Authorization  %s \n", Authorization.c_str());
 
+    // uWS库使用sample
     uWS::Hub h;
     h.onMessage([](uWS::WebSocket<uWS::CLIENT> *ws, char *message, size_t length, uWS::OpCode opCode) {
       std::cout << std::string(message, length) << std::endl;
@@ -206,6 +248,7 @@ void WebSocketSample(){
     //extraHeaders2["Host"]=server;
     extraHeaders2["hobot_xpush_client_id"]="cid";
     std::string ws_uri = "ws://xpushservice-aiot.horizon.ai/ws?authorization=" + Authorization;
+    // http请求需要添加之前的params
     addquery(ws_uri, params);
     h.connect(ws_uri, nullptr, extraHeaders2);
     h.run();
